2c7427ef50bb8a78cef8ba304e90d4d63fb58e8a
Chris Randle <crandle@DX1T6LM02.digitalglobe.com>
2015-08-05 11:39:11 -0600

    script cleanup

--- stop_tomcat.sh
+++ stop_tomcat.sh
@@ -1,5 +1,4 @@
 
-#tomcat_pid=`ps ax | grep tomcat | grep 'Bootstrap start' | sed -e 's/^[[:space:]]*//' | cut -d' ' -f1`
 tomcat_pid=$(get_tomcat_pid.sh)
 if [ ! -z "$tomcat_pid" -a "$tomcat_pid" != "" ]; then
     echo "Stopping Tomcat with pid: $tomcat_pid"
@@ -8,6 +7,5 @@ fi
 
 while [ ! -z "$tomcat_pid" -a "$tomcat_pid" != "" ]; do
     sleep 1;
-    #tomcat_pid=`ps ax | grep tomcat | grep 'Bootstrap start' | sed -e 's/^[[:space:]]*//' | cut -d' ' -f1`
     tomcat_pid=$(get_tomcat_pid.sh)
 done
